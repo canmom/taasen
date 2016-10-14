6934a38bc98dea891f07c9be80f770d01e5f2f79
canmom <bryn.dickinson@gmail.com>
2016-10-14 23:52:12 +0100

    Changed single tile to row from fixed list.

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,6 @@
 <template>
-<svg width="600" height="600" viewBox="0 0 100 100">
-  <tile :x="tileCentre.x" :y="tileCentre.y" :up="+1" :r="radius"></tile>
+<svg width="600" height="600" viewBox="0 0 4 4">
+  <tile v-for="tile in positions" :x="tile.x" :y="tile.y" :up="+1" :r="radius"></tile>
 </svg>
 </template>
 
@@ -10,8 +10,8 @@ import Tile from './Tile'
 export default {
   data: function () {
     return {
-      tileCentre: { x: 50, y: 50 },
-      radius: 30
+      positions: [{x: 0.5, y: 3}, {x: 1.5, y: 3}, {x: 2.5, y: 3}, {x: 3.5, y: 3}],
+      radius: 1 / Math.sqrt(3)
     }
   },
   components: {
