18ec80c5754639a8f20e4a54be2b340fdd88c067
canmom <bryn.dickinson@gmail.com>
2016-10-15 13:02:31 +0100

    Add skeleton of Piece component.

---
+++ src/components/Piece.vue
@@ -0,0 +1,27 @@
+<template>
+<g :class='elClass' >
+
+</g>
+</template>
+
+<script>
+export default {
+  props: {
+    piece: {
+      validator: function (value) {
+        return new Set(['thaum','sciane','paupil']).has(value)
+      }
+    }
+    faction: {
+      validator: function(value) {
+        return new Set(['red','green','blue']).has(value)
+      }
+    }
+  }
+  computed: {
+    elClass: function () {
+      return `piece ${piece} ${faction}`
+    }
+  }
+}
+</script>
