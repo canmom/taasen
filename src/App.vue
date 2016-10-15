d55743409d864660837ae91a11e5789452730c3b
canmom <bryn.dickinson@gmail.com>
2016-10-15 01:09:09 +0100

    Style background and add title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <header>
+      <h1>Taasen</h1>
+      <p>The game of Tryslmaistan chess</p>
+    </header>
     <board></board>
   </div>
 </template>
@@ -17,12 +21,36 @@ export default {
 <style>
 html {
   height: 100%;
+
 }
 
 body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+  background: linear-gradient( to bottom, #96F6F6, #117985);
+  background-repeat: no-repeat;
+}
+
+header {
+  position: absolute;
+  margin-left: 40px;
+  margin-top: 20px;
+}
+
+header h1 {
+  font-family: "Linux Libertine Display",serif;
+  font-weight: normal;
+  font-size: 80px;
+  margin:0;
+}
+
+header p {
+  margin-top:-5px;
+  margin-left:60px;
+  font-family: "Linux Biolinum", sans-serif;
+  font-size: 25px;
+  font-style: italic;
+}
+
+#app {
+  width: 100%;
 }
 </style>
