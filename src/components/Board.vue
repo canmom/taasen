5776c931aaf303b89cee26bd72e4e930542f1be6
canmom <bryn.dickinson@gmail.com>
2016-10-16 11:53:19 +0100

    Add functions and display for checking whose turn it is to move

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,7 @@
 <template>
-<div class="board">
-  <svg width="600" height="600" viewBox="0 0 4 4">
+<div class="game">
+  <move-indicator :faction="toMove"></move-indicator>
+  <svg class="board" width="600" height="600" viewBox="0 0 4 4">
     <tile v-for="tile in tiles" :x="tile.x" :y="tile.y" :up="tile.u" :r="tileRadius" :t="tile.t"></tile>
     <destination-overlay v-for="(dest,destLabel) in destinations" :x="dest.x" :y="dest.y" :up="dest.u" :r="tileRadius" v-on:move="movePiece(destLabel)"></destination-overlay>
     <piece v-for='(piece, pieceIndex) in pieces' :loc='piece.loc' :r='pieceRadius' :piece='piece.piece' :faction='piece.faction' :starting='piece.starting' :selected='piece.selected' v-on:select='beginMoving(pieceIndex)'></piece>
@@ -13,6 +14,7 @@ import Tile from './Tile'
 import {tiles, tileRadius} from './Tiles'
 import Piece from './Piece'
 import DestinationOverlay from './DestinationOverlay'
+import MoveIndicator from './MoveIndicator'
 
 export default {
   data: function () {
@@ -40,7 +42,8 @@ export default {
   components: {
     Tile,
     Piece,
-    DestinationOverlay
+    DestinationOverlay,
+    MoveIndicator
   },
   methods: {
     resetMoving: function () {
@@ -54,6 +57,7 @@ export default {
       this.pieces[this.moving].loc = destination
       this.pieces[this.moving].starting = false
       this.resetMoving()
+      this.toMove = this.toMove === 'red' ? 'green' : 'red'
     },
     showDestinations: function (tileLabel) {
       for (let label of tiles[tileLabel].a) {
@@ -66,18 +70,24 @@ export default {
     },
     beginMoving: function (pieceIndex) {
       this.resetMoving()
-      this.moving = pieceIndex
-      var tileLabel = this.pieces[pieceIndex].loc
-      this.pieces[pieceIndex].selected = true
-      this.showDestinations(tileLabel)
+      if (this.pieces[pieceIndex].faction === this.toMove || this.pieces[pieceIndex].faction === 'blue') {
+        this.moving = pieceIndex
+        var tileLabel = this.pieces[pieceIndex].loc
+        this.pieces[pieceIndex].selected = true
+        this.showDestinations(tileLabel)
+      }
     }
   }
 }
 </script>
 
 <style>
+.game {
+  width: 100%;
+}
 .board {
+  width: 600;
   margin: 0 auto;
-  width: max-content;
+  display: block;
 }
 </style>
---
+++ src/components/MoveIndicator.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="indicator">
+  <p>
+    <svg width="50" height="50" viewBox="0 0 1 1"> 
+      <rect class="indicator-box" x="0.1" y="0.1" width="0.8" height="0.8" rx="0.3" ry="0.3" :style='style'/>
+    </svg>
+    <strong>{{faction}}</strong> to move
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    style: function () {
+      return {
+        fill: this.faction === 'red' ? '#FF4452' : '#64BC0D'
+      }
+    }
+  },
+  props: {
+    faction: {
+      validator: function (value) {
+        return value === 'red' || value === 'green'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.indicator {
+  font-family: "Linux Biolinum", sans-serif;
+  font-size: 30px;
+  width: max-content;
+  position: absolute;
+  right: 80px;
+  top: 20px;
+}
+.indicator p {
+  margin: 0;
+}
+.indicator svg {
+  display: inline-block;
+  margin-bottom: -18px;
+  margin-right: 5px;
+}
+.indicator-box {
+  stroke: black;
+  stroke-width: 0.1;
+}
+.indicator text {
+
+}
+</style>
