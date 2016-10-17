bcee99a80b57deaa7f55b36a5c0d6336b7b52ed2
canmom <bryn.dickinson@gmail.com>
2016-10-17 22:16:57 +0100

    Create single source for faction colours

---
+++ src/components/Colours.js
@@ -0,0 +1,12 @@
+export default {
+  outer: {
+    red: '#FF4452',
+    green: '#64BC0D',
+    blue: '#23F1FF'
+  },
+  inner: {
+    red: '#820A13',
+    green: '#306000',
+    blue: '#00757C'
+  }
+}
