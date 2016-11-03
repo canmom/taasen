81b8a8623ec021e68373872215a61211a0165afb
canmom <bryn.dickinson@gmail.com>
2016-11-03 16:06:21 +0000

    Added footer giving credits.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
       <p>the game of Tryslmaistan chess</p>
     </header>
     <board></board>
+    <footer>
+      <p><a href='http://unicornjelly.com/taasen1.html'>original rules</a> by <a href='http://jenniverse.com/'>Jennifer Diane Reitz</a></p>
+      <p><a href='https://vuejs.org/'>Vue.js</a> implementation by <a href='https://github.com/canmom'>canmom</a></p>
+      <p>fonts by <a href='http://www.linuxlibertine.org'>Linux Libertine</a></p>
+    </footer>
   </div>
 </template>
 
@@ -45,6 +50,30 @@ header p {
   font-family: "Linux Biolinum", sans-serif;
   font-size: 25px;
   font-style: italic;
+}
+
+footer {
+  position: absolute;
+  bottom: 15px;
+  right: 20px;
+}
+
+footer p {
+  color:  white;
+  font-family: "Linux Biolinum", sans-serif;
+  font-size: 20px;
+  margin: 0;
+  text-align:  right;
+}
+
+footer a {
+  text-decoration: none;
+  color: #bff;
+  font-weight: bold;
+}
+
+footer a:hover {
+  color: #8dd;
 }
 
 #app {
